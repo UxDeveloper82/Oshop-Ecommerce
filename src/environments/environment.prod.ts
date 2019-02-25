eb4a8698096eb0a9e4c81f0dadc1cd6fd88b1b70
electronicsmf <electronicsmf@aol.com>
2019-02-24 19:32:09 -0500

    Initial commit Angular 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+     apiKey: 'AIzaSyAesHDSpf8LEEnlObtB5h4QXlAq3agS01w',
+    authDomain: 'oshop-d90ae.firebaseapp.com',
+    databaseURL: 'https://oshop-d90ae.firebaseio.com',
+    projectId: 'oshop-d90ae',
+    storageBucket: 'oshop-d90ae.appspot.com',
+    messagingSenderId: '933303167167'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+     apiKey: 'AIzaSyAesHDSpf8LEEnlObtB5h4QXlAq3agS01w',
+    authDomain: 'oshop-d90ae.firebaseapp.com',
+    databaseURL: 'https://oshop-d90ae.firebaseio.com',
+    projectId: 'oshop-d90ae',
+    storageBucket: 'oshop-d90ae.appspot.com',
+    messagingSenderId: '933303167167'
+  }
 };
