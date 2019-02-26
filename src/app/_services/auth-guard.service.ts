364d0afd11d0002c2fa76fea2798081e9c4f5399
electronicsmf <electronicsmf@aol.com>
2019-02-25 19:56:48 -0500

    Store users in database

--- src/app/_services/auth-guard.service.ts
+++ src/app/_services/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from './auth.service';
 import 'rxjs/add/operator/map';
 
@@ -10,12 +10,12 @@ export class AuthGuard implements CanActivate {
 
     constructor(private auth: AuthService, private router: Router) {}
 
-    canActivate() {
+    canActivate(route, state: RouterStateSnapshot) {
         return this.auth.user$.map(user => {
            // tslint:disable-next-line:curly
            if (user) return true;
 
-           this.router.navigate(['/login']);
+           this.router.navigate(['/login'], { queryParams: {returnUrl: state.url }});
            return false;
         });
     }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { AdminProductsComponent } from './admin/admin-products/admin-products.co
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { AuthGuard } from './_services/auth-guard.service';
+import { UserService } from './_services/user.service';
 
 @NgModule({
   declarations: [
@@ -57,7 +58,8 @@ import { AuthGuard } from './_services/auth-guard.service';
   ],
   providers: [
     AuthService,
-    AuthGuard
+    AuthGuard,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
